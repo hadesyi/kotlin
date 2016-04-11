1f290e034350a5661909fffe90ed5617cd4cabe2
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2016-04-11 19:42:13 +0300

    Code Insight: Support file path references inside of Kotlin string literals  #KT-11704 Fixed

---
+++ idea/testData/resolve/references/fileRefInRawStringLiteral.kt
@@ -0,0 +1,5 @@
+fun foo() {
+    val s = """idea/testData/resolve/references/<caret>fileRefInRawStringLiteral.Data.java"""
+}
+
+// REF: /src/idea/testData/resolve/references.fileRefInRawStringLiteral.Data.java
