21b7c63e492a8cff5b19787b9154a3dbc364785f
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-05-19 14:30:34 +0300

    Generate defaults methods on extending jvm6 interfaces

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ImplementationBodyCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ImplementationBodyCodegen.java
@@ -1314,7 +1314,7 @@ public class ImplementationBodyCodegen extends ClassBodyCodegen {
     }
 
     private void generateTraitMethods() {
-        if (isInterface(descriptor)) return;
+        if (isJvm6Interface(descriptor, state)) return;
 
         for (Map.Entry<FunctionDescriptor, FunctionDescriptor> entry : CodegenUtil.getNonPrivateTraitMethods(descriptor).entrySet()) {
             FunctionDescriptor traitFun = entry.getKey();
